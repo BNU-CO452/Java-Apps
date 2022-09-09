14a8a7072f0c4eb9aafcb3b0b0fe11d110544575
Derek Peacock <derek.peacock@yahoo.com>
2022-09-09 11:14:51 +0100

    Renamed week 1

--- Week1/Week1.java
+++ Week1/Week1.java
@@ -1,11 +1,15 @@
-public class Hello 
+public class Week1 
 {
     public static final String CONSOLE_CLEAR = "\033[H\033[2J";
     public static void main(String[] args)
     {
         System.out.println(CONSOLE_CLEAR);
+
+        System.out.println("==================================");
         System.out.println("CO452 Programming Concepts 2022/23");
         System.out.println("==================================");
+        System.out.println();
+        System.out.println("  Week 1 Java Basic Input/Output ");
         System.out.println();
         System.out.println(" by Nicholas Day and Derek Peacock");
         System.out.println();
