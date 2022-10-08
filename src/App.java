1d485244af163d1745a1ac3f2df83539a5c46d36
Derek Peacock <derek.peacock@yahoo.com>
2022-10-08 14:29:37 +0100

    Changed colours

--- src/App.java
+++ src/App.java
@@ -4,8 +4,7 @@ public class App
 {
     public static void main(String[] args) throws Exception 
     {
-        System.out.println(ConsoleColours.ANSI_BRIGHT_BG_PURPLE);
-        System.out.println(ConsoleColours.ANSI_BRIGHT_BG_YELLOW);
+        System.out.println(ConsoleColours.ANSI_BRIGHT_BG_BLUE);
         System.out.println(ConsoleColours.CONSOLE_CLEAR);
 
         System.out.println("CO452 Programming Concepts 2022/23");
